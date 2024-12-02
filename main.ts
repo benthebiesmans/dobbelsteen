a3b96a3de3020e9c1542743e9653141354feaa2b
benthebiesmans <benthebiesmans@gmail.com>
2024-12-02 15:06:25 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+let getal = 0
+input.onGesture(Gesture.Shake, function () {
+    getal = randint(1, 6)
+    basic.showNumber(getal)
+    basic.pause(2000)
+    basic.clearScreen()
 })
